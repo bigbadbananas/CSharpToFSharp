8ec7dc6e36ae88e45a767b4c1cd047c088420e4d
Phillip <phillip-carter@jmstudios.net>
2014-07-02 19:47:38 -0600

    Initial Commit

---
+++ CSharpToFSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientUtils.FsharpLib;
+
+namespace CSharpToFSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int ans1 = SomeMathShit.factorial1(6);
+            int ans2 = SomeMathShit.factorial2(6);
+            int ans3 = SomeMathShit.factorial3(6);
+            int ans4 = SomeMathShit.factorial4(6);
+
+            Console.WriteLine("First F# factorial: " + ans1);
+            Console.WriteLine("Second F# factorial: " + ans2);
+            Console.WriteLine("Third F# factorial: " + ans3);
+            Console.WriteLine("Fourth F# factorial: " + ans4);
+        }
+    }
+}
---
+++ FSharpTest/Library1.fs
@@ -0,0 +1,25 @@
+﻿namespace ClientUtils.FsharpLib
+
+module SomeMathShit = 
+    
+    // Naive recursion, not optimized
+    let rec factorial1 n =
+        match n with
+        | 0 | 1 -> 1
+        | _ -> n * factorial1 (n - 1)
+
+    // Uses tail-recursion, optimized
+    let factorial2 n = 
+        let rec loop i acc = 
+            match i with
+            | 0 | 1 -> acc
+            | _ -> loop (i - 1) (acc * i)
+        loop n 1
+
+    // Uses tail-recursion internally, but instead as built-in list/seq function
+    let factorial3 n = 
+        [1..n] |> List.fold (*) 1
+
+    // Same as factorial3, but doesn't require second parameter
+    let factorial4 n = 
+        [1..n] |> List.reduce (*)
